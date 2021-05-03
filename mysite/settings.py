9079bb5002e059c61c395648820807fd2f59e434
N.Sai Pranavdhar Reddy <73348574+iampranavdhar@users.noreply.github.com>
2021-05-03 12:26:35 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [       
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'http://127.0.0.1:8000'
+    'http://127.0.0.1:8000'.
+    'https://member-details-pranav.herokuapp.com'
 ]
 
 GRAPHENE = {
